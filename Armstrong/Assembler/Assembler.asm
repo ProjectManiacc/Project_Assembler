dc26d6ed2217b663be87e93c33ebe69318560c83
heracro <heracro@gmail.com>
2024-01-17 19:40:02 +0100

    Update Assembler.asm
    Fixed power function for exponents 2...6 (incl.). It multiply (exp-1) times 4 2B-numbers (9^6 fits in 2 bytes).

--- Armstrong/Assembler/Assembler.asm
+++ Armstrong/Assembler/Assembler.asm
@@ -1,23 +1,28 @@
+.model flat, stdcall
+option casemap:none
+
+.data
+    vector      DWORD 0, 0, 0, 0          ; Placeholder for 4 DWORD parameters (vector elements)
+    exponent    DWORD 0                    ; Exponent parameter
+
 .code
-ALIGN 16
+    PUBLIC asm_power
 
-PUBLIC asm_power
-asm_power PROC base:QWORD, exponent:QWORD
-    mov rax, 1         ; Initialize the result to 1
+asm_power PROC
+    mov ecx, [exponent]                 ; Load the exponent into ecx
+    movdqu xmm0, [vector]               ; Load 4 DWORD parameters (vector elements) into xmm0
+    movaps xmm1, xmm0                   ; Copy the vector elements to xmm1
 
-    ; Handling the power operation (base^exponent)
-    mov rcx, exponent  ; Load the exponent into rcx
+    ; Calculate xmm0 ^ ecx (raise each element in xmm0 to the power of ecx)
+    power_loop:
+        dec ecx                         ; Decrement loop counter
+        jz  done                        ; If ecx is zero, jump to the end of the loop
+        imulps xmm0, xmm1               ; Multiply xmm0 by xmm1 (result in xmm0)
+        jmp power_loop
 
-power_loop:
-    cmp rcx, 0          ; Check if the exponent is equal to 0
-    je  power_done      ; If yes, exit the loop with result 1
-
-    imul rax, base      ; Multiply the result by the base
-    dec rcx             ; Decrement the exponent
-    jmp power_loop      ; Repeat the loop
-
-power_done:
-    ; The result of the power operation is stored in the rax register
+    done:
+    ; The result is in xmm0
+    movd [result], xmm0                ; Store the result in the result variable
     ret
 asm_power ENDP
 
