6013f2f894d344cba3dda041130f7b3ca295b9ba
Piotr Kluziok <piotrkluziok@gmail.com>
2024-01-28 12:29:02 +0100

    Changed types from short to int

--- Armstrong/ArmstrongGUI/Digits2B.cs
+++ Armstrong/ArmstrongGUI/Digits2B.cs
@@ -2,34 +2,36 @@ using System.Collections.Generic;
 
 namespace ArmstrongGUI
 {
-  /** This class helps to store 4-elements array of 2 byte numbers.
-  It's used to pass these arrays to assembler function for vector multiplying
-  and later collect results and sum them up.
-  */
-  public class Digits2B
+    /** This class helps to store 4-elements array of 2 byte numbers.
+    It's used to pass these arrays to assembler function for vector multiplying
+    and later collect results and sum them up.
+    */
+    public class Digits2B
     {
-        public short CountOfFours { get; private set; }
-    
-        public List<short[]> Fours { get; }
-    
+        public int CountOfFours { get; private set; }
+
+        public List<int[]> Fours { get; }
+
+
+
         public Digits2B(List<int> list)
         {
-            CountOfFours = (short)((list.Count - 1) / 4 + 1);
-            Fours = new List<short[]>();
+            CountOfFours = ((list.Count - 1) / 4 + 1);
+            Fours = new List<int[]>();
             for (int i = 0; i < CountOfFours - 1; ++i)
             {
-                short[] temp = {  (short)list[4*i],
-                                  (short)list[4*i + 1],
-                                  (short)list[4*i + 2],
-                                  (short)list[4*i + 3]
+                int[] temp = {  list[4*i],
+                                 list[4*i + 1],
+                                 list[4*i + 2],
+                                 list[4*i + 3]
                 };
                 Fours.Add(temp);
             }
             int startingNum = (CountOfFours - 1) * 4; //8
-            short[] lastPiece = { 0, 0, 0, 0 };
+            int[] lastPiece = { 0, 0, 0, 0 };
             for (int i = startingNum; i < list.Count; ++i)
             {
-                lastPiece[i - startingNum] = (short)list[i];
+                lastPiece[i - startingNum] = list[i];
             }
             Fours.Add(lastPiece);
         }
@@ -37,25 +39,14 @@ namespace ArmstrongGUI
         public Digits2B()
         {
             CountOfFours = 0;
-            Fours = new List<short[]>();
+            Fours = new List<int[]>();
         }
 
-        public void Add(short[] results)
+        public void Add(int[] results)
         {
             ++CountOfFours;
             Fours.Add(results);
         }
-        public int Sum()
-        {
-            int sum = 0;
-            for (int i = 0; i < CountOfFours; ++i)
-            {
-                for (int j = 0; j < 4; ++j)
-                {
-                    sum += (int)Fours[i][j];
-                }
-            }
-            return sum;
-        }
+
     }
 }
